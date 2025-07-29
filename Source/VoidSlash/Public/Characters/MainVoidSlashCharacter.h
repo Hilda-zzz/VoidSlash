2125fa5f67c45e857be7bcbec1564d2d2ffcf899
Hilda <heye1010@126.com>
2025-07-28 19:29:11 -0500

    Update cpp files for player animations

---
+++ Source/VoidSlash/Public/Characters/MainVoidSlashCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VoidSlashCharacter.h"
+#include "MainVoidSlashCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOIDSLASH_API AMainVoidSlashCharacter : public AVoidSlashCharacter
+{
+	GENERATED_BODY()
+	
+};

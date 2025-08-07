39a030f3c245d0bde86439db244b249b82d56173
Hilda <heye1010@126.com>
2025-08-07 01:00:41 -0500

    Trace Component

--- Source/VoidSlash/Private/Combat/TraceComponent.cpp
+++ Source/VoidSlash/Private/Combat/TraceComponent.cpp
@@ -19,8 +19,7 @@ void UTraceComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	SkeletalComp = GetOwner()->FindComponentByClass<USkeletalMeshComponent>();
 }
 
 
@@ -29,6 +28,46 @@ void UTraceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActor
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FVector StartSocketLocation{ SkeletalComp->GetSocketLocation(Start) };
+	FVector EndSocketLocation{ SkeletalComp->GetSocketLocation(End) };
+	FQuat ShapeRotation{ SkeletalComp->GetSocketQuaternion(Rotation) };
+
+	TArray<FHitResult> OutResults;
+	double WeaponDistance(
+		FVector::Distance(StartSocketLocation, EndSocketLocation)
+	);
+	FVector BoxHalfExtent{
+		BoxCollisionLength,BoxCollisionLength,WeaponDistance
+	};
+	BoxHalfExtent /= 2;
+	FCollisionShape Box{
+		FCollisionShape::MakeBox(BoxHalfExtent)
+	};
+
+	FCollisionQueryParams IgnoreParams{
+		FName{TEXT("Igonre Params")},
+		false,
+		GetOwner()
+	};
+
+	bool bHasFoundTargets{
+		GetWorld()->SweepMultiByChannel(
+		OutResults,
+		StartSocketLocation,
+		EndSocketLocation,
+		ShapeRotation,
+		ECollisionChannel::ECC_GameTraceChannel1,
+		Box,
+		IgnoreParams
+	)};
+
+	if (bHasFoundTargets)
+	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("Target Found!")
+		)
+	}
 }
 
